a77e36d982d13c8e6996c6d781970d53daafabcd
agustina-aliciardi <agustina.aliciardi@darwoft.com>
2025-04-02 16:21:06 -0300

    se agrgeo ejercicio de valores predeterminados

---
+++ ejercicio2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var (
+		defaultInt int
+		defaultUint uint
+		defaultFloat32 float32
+		defaultBool bool
+		defaultString string
+	)
+
+	fmt.Println(defaultInt, defaultUint, defaultFloat32, defaultBool, defaultString)
+}
