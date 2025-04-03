ca8b9d31c77fc4209f5648a2cb3469e3a1385c18
agustina-aliciardi <agustina.aliciardi@darwoft.com>
2025-04-03 16:08:14 -0300

    Se agrego ejercicio 4

--- ejercicio4/main.go
+++ ejercicio4/main.go
@@ -11,40 +11,39 @@ func main(){
 
 func play(){
 	numAleatorio := rand.Intn(100)
-	var canIntentos int
 	var numIngresado int
 
-	for canIntentos < 10 {
-		canIntentos++
-		fmt.Printf("Ingrese un número (intentos restantes %d): ", 10-canIntentos+1)
+	for cantIntentos := 0 ; cantIntentos<10 ; cantIntentos++ {
+		fmt.Printf("Ingrese un número (intentos restantes %d): ", 10-cantIntentos)
 		fmt.Scanln(&numIngresado)
 
 		if numIngresado == numAleatorio {
 			fmt.Println("Felicitaciones Ganaste!")
 			displayMenu()
 			return
-		} else if numIngresado>numAleatorio{
+		}else if numIngresado > numAleatorio {
 			fmt.Println("No acertaste. El número es menor.")
-		}else {
+		}else{
 			fmt.Println("No acertaste. El número es mayor.")
 		}
 	}
-	fmt.Println("Se acabaron los intentos. El número es: ", numAleatorio)
+	fmt.Println("Se acabaron los intentos. El números es: ", numAleatorio)
 	displayMenu()
 }
 
 func displayMenu(){
 	var option string
-	fmt.Print("Desea jugar nuevamente? (s/n): ")
+
+	fmt.Println("Desea jugar nuevamente? (s/n): ")
 	fmt.Scanln(&option)
 
 	switch option{
 	case "s":
 		play()
 	case "n":
-		fmt.Println("Gracias por jugar!")
+		fmt.Println("Gracias por jugar")
 	default:
-		fmt.Println("Opción incorrecta. Intente de Nuevo!")
+		fmt.Println("Opcion incorrecta. Inente de nuevo!")
 		displayMenu()
 	}
 }
