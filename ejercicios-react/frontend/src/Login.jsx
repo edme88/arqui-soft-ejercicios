52d83aa637ffd877d79dcd1c06c92144f232f372
agustina-aliciardi <agustina.aliciardi@darwoft.com>
2025-05-15 19:20:43 -0300

    react login basico

---
+++ ejercicios-react/frontend/src/Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./Login.css";
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        if(username=="admin" && password=="admin"){
+            console.log("Login OK");
+        }else{
+            console.log("Login Incorrecto");
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleLogin}>
+                <h2>
+                    Iniciar Sesión
+                </h2>
+                <input
+                    type="text"
+                    placeholder="Usuario"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Contraseña"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                />
+                <button type="submit">Ingresar</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
